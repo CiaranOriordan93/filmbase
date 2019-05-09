f681cc0635ee8cd7bf2ca1a913636de36047173d
ciaran oriordan <ciaranoriordan93@gmail.com>
2019-05-09 15:01:58 +0100

    loaded sass and began NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view />
+    <NavBar />
   </div>
 </template>
 
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+html {
+  font-size: 62.5%;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  font-family: "Nunito", sans-serif;
+  font-weight: 400;
+  color: #fff;
+  line-height: 1.7;
+}
+
+button,
+input[type="submit"],
+input[type="reset"] {
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
 }
 </style>
